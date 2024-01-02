b6ada6922aaf193ebd861cdef1596b2cad38cc01
eswar ramakurthi <147490953+ramakurthieswar@users.noreply.github.com>
2024-01-02 21:19:46 +0530

    #include<stdio.h> int main() {     int a,i,j;     scanf("%d",&a);     for(i=a;i>=1;i--)     {         for(j=1;j<=a;j++)         {             if(j>i)             printf("*");             else             printf("%d",i);         }printf(" ");     } }

---
+++ Basic_Pattern_5.3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,j;
+    scanf("%d",&a);
+    for(i=a;i>=1;i--)
+    {
+        for(j=1;j<=a;j++)
+        {
+            if(j>i)
+            printf("*");
+            else
+            printf("%d",i);
+        }printf("
+");
+    }
+}
