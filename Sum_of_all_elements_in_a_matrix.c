5b8c1d63a43a4062060229c1634ff0797deac8dd
eswar ramakurthi <147490953+ramakurthieswar@users.noreply.github.com>
2024-01-04 21:38:25 +0530

    #include<stdio.h> int main() {     int r,c;     scanf("%d%d",&r,&c);     int a[r][c],i,j,s=0;     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&a[i][j]);             s=s+a[i][j];         }     }     printf("%d",s); }

---
+++ Sum_of_all_elements_in_a_matrix.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int r,c;
+    scanf("%d%d",&r,&c);
+    int a[r][c],i,j,s=0;
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&a[i][j]);
+            s=s+a[i][j];
+        }
+    }
+    printf("%d",s);
+}
